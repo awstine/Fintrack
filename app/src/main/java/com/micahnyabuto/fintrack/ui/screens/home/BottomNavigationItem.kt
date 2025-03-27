fb63a9f6d1390e0544027e66136bd5056f8f2fe0
Остин Джесси <116760268+awstine@users.noreply.github.com>
2025-03-27 23:31:47 +0100

    updated the bottombar

--- app/src/main/java/com/micahnyabuto/fintrack/ui/screens/home/BottomNavigationItem.kt
+++ app/src/main/java/com/micahnyabuto/fintrack/ui/screens/home/BottomNavigationItem.kt
@@ -4,42 +4,17 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import com.micahnyabuto.fintrack.R
 
 data class BottomNavigationItem(
     val title :String,
-    val selectedIcon: ImageVector,
-    val unselectedIcon: ImageVector,
+    val selectedIcon: Painter,
+    val unselectedIcon: Painter,
 )
 
-val items= listOf(
-    BottomNavigationItem(
-        title = "",
-        selectedIcon = Icons.Filled.Home,
-        unselectedIcon = Icons.Filled.Home,
-    ),
-    BottomNavigationItem(
-        title = "",
-        selectedIcon = Icons.Filled.Person,
-        unselectedIcon = Icons.Filled.Person,
-    ),
-
-    BottomNavigationItem(
-        title = "",
-        selectedIcon = Icons.Filled.Add,
-        unselectedIcon = Icons.Filled.Add,
-        //shape = circleshape
-    ),
-
-    BottomNavigationItem(
-        title = "",
-        selectedIcon = Icons.Filled.Person,
-        unselectedIcon = Icons.Filled.Person,
-    ),
-
-    BottomNavigationItem(
-        title = "",
-        selectedIcon = Icons.Filled.Person,
-        unselectedIcon = Icons.Filled.Person,
-    ),
-)
