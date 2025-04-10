38509078929e977ec79ea38d9f117c274f417731
jaba limpepe <awstinejessy@gmail.com>
2025-04-10 17:17:11 +0300

    Create Destinations.kt

---
+++ app/src/main/java/com/micahnyabuto/fintrack/ui/navigation/Destinations.kt
@@ -0,0 +1,11 @@
+package com.micahnyabuto.fintrack.ui.navigation
+
+object Destinations {
+    const val ONBOARDING = "onboarding_screen"
+    const val FINTRACK = "fintrack"
+    const val WALLET_SCREEN = "wallet_screen"
+    const val ADD_EXPENSE_SCREEN = "add_expense_screen"
+    const val STATISTICS_SCREEN = "statistics_screen"
+    const val PROFILE_SCREEN = "profile_screen"
+    const val SETTINGS_SCREEN = "settings_screen"
+}
