16bba827fc32f147ba636240f490c535836f7ac5
jaba limpepe <awstinejessy@gmail.com>
2025-04-10 17:16:50 +0300

    Create Days.kt

---
+++ app/src/main/java/com/micahnyabuto/fintrack/data/Days.kt
@@ -0,0 +1,16 @@
+package com.micahnyabuto.fintrack.data
+
+data class Days(
+    val day: String,
+)
+
+val days =
+    listOf(
+        Days("Monday"),
+        Days("Tuesday"),
+        Days("Wednesday"),
+        Days("Thursday"),
+        Days("Friday"),
+        Days("Saturday"),
+        Days("Sunday"),
+    )
