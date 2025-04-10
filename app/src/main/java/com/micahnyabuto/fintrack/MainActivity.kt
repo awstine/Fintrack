056a024126b6bac1214f6d44e56444acb39c5046
jaba limpepe <awstinejessy@gmail.com>
2025-04-10 17:16:57 +0300

    Update MainActivity.kt

--- app/src/main/java/com/micahnyabuto/fintrack/MainActivity.kt
+++ app/src/main/java/com/micahnyabuto/fintrack/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.micahnyabuto.fintrack
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import com.example.compose.FintrackTheme
-import com.micahnyabuto.fintrack.ui.screens.expense.AddExpenseScreen
-import com.micahnyabuto.fintrack.ui.screens.home.FintrackApp
+import com.micahnyabuto.fintrack.ui.navigation.AppRoute
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FintrackTheme {
-                FintrackApp()
+                AppRoute()
             }
         }
     }
